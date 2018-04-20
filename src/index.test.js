61c795fd003788ee3f12543beef3ede21543fd7c
Juliano Rafael <juliano@getty.io>
2018-04-20 11:08:53 -0300

    Fix render test and update snapshot

--- src/index.test.js
+++ src/index.test.js
@@ -5,6 +5,14 @@ import Form from './'
 import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Form />).toJSON()
+  const tree = renderer.create(<Form
+    schema={[
+      {
+        name: 'input1',
+        type: 'text',
+        label: 'Input 1'
+      }
+    ]}
+  />).toJSON()
   expect(tree).toMatchSnapshot()
 })
