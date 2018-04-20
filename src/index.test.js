3b1bac917d2593a954861517793e90dce9583d44
Juliano Rafael <juliano@getty.io>
2018-04-20 14:49:06 -0300

    Add Form tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,127 @@
 import 'react-native'
+import { Text } from 'react-native'
 import React from 'react'
+import { shallow } from 'enzyme'
+
 import Form from './'
+import Input from './Input'
+import { Row, RowItem } from './Utils'
 
-import renderer from 'react-test-renderer'
+import './testSetup'
 
-it('renders correctly', () => {
-  const tree = renderer.create(<Form
-    schema={[
-      {
-        name: 'input1',
-        type: 'text',
-        label: 'Input 1'
-      }
-    ]}
-  />).toJSON()
-  expect(tree).toMatchSnapshot()
+describe('Form', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(
+      <Form
+        schema={[
+          {
+            name: 'input1',
+            type: 'text',
+            label: 'Input 1'
+          }
+        ]}
+      />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders one input', () => {
+    const wrapper = shallow(
+      <Form
+        schema={[
+          {
+            name: 'input1',
+            type: 'text',
+            label: 'Input 1'
+          }
+        ]}
+      />
+    )
+    expect(wrapper.find(Input)).toHaveProperty('length', 1)
+  })
+
+  it('renders a custom input', () => {
+    const CustomInput = () => <Text>CustomInput</Text>
+    const wrapper = shallow(
+      <Form
+        schema={[
+          {
+            name: 'input1',
+            type: CustomInput,
+            label: 'Input 1'
+          }
+        ]}
+      />
+    )
+    expect(wrapper.find(Input)).toHaveProperty('length', 1)
+  })
+
+  it('renders two inputs', () => {
+    const wrapper = shallow(
+      <Form
+        schema={[
+          {
+            name: 'input1',
+            type: 'text',
+            label: 'Input 1'
+          },
+          {
+            name: 'input2',
+            type: 'text',
+            label: 'Input 2'
+          }
+        ]}
+      />
+    )
+    expect(wrapper.find(Input)).toHaveProperty('length', 2)
+  })
+
+  it('renders two inputs in the same row', () => {
+    const wrapper = shallow(
+      <Form
+        schema={[
+          [
+            {
+              name: 'input1',
+              type: 'text',
+              label: 'Input 1'
+            },
+            {
+              name: 'input2',
+              type: 'text',
+              label: 'Input 2'
+            }
+          ]
+        ]}
+      />
+    )
+    expect(wrapper.find(Input)).toHaveProperty('length', 2)
+  })
+
+  it('renders two inputs in the same row with different length', () => {
+    const schema = [
+      [
+        {
+          name: 'input1',
+          type: 'text',
+          label: 'Input 1',
+          flex: 1
+        },
+        {
+          name: 'input2',
+          type: 'text',
+          label: 'Input 2',
+          flex: 2
+        }
+      ]
+    ]
+    const wrapper = shallow(
+      <Form
+        schema={schema}
+      />
+    )
+    wrapper.find(Row).children().forEach((node, index) => {
+      expect(node.find(RowItem).props().flex).toEqual(schema[0][index].flex)
+    })
+  })
 })
